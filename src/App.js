f343ed6c30dabc3c78db704aef4e102dae555b1e
erwinmarin05@gmail.com <erwinmarin05@gmail.com>
2023-10-15 06:14:23 +0800

    Render details cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
 import NavBar from './components/NavBar';
-import Hero from './components/Hero'
+import Hero from './components/Hero';
+import Cards from './components/Cards'
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Cards
+        img ="../image/swimming_v2.png"
+        rating ="5.0 (6)"
+        location =". USA"
+        name = "Life lessons with Katie Zaferes"
+        price = "From $136"
+      />
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-export default function Cards (){
+export default function Cards (props){
     return(
         <div>
-            <h2>Cards here</h2>
+            <section className="Card--Section">
+                 <img className="Image--Card" src= {props.img}  alt="swimming"/>
+                 <div>
+                 <img className="Image--star" src ="../image/Star.png" alt="star" />  
+                 <span className="Rating--Loc">{props.rating} {props.location}</span>
+                 <p>{props.name}</p>
+                 <p>{props.price} / person</p>                  
+                </div> 
+                 
+            </section>
+            
         </div>
     )
 }
