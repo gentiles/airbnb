bd0b627b9abd47bd257bc567f1d56a6b7c825ac6
erwinmarin05@gmail.com <erwinmarin05@gmail.com>
2023-10-15 06:44:52 +0800

    fix line height of Cards text

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,12 +5,14 @@ export default function Cards (props){
         <div>
             <section className="Card--Section">
                  <img className="Image--Card" src= {props.img}  alt="swimming"/>
-                 <div>
-                 <img className="Image--star" src ="../image/Star.png" alt="star" />  
-                 <span className="Rating--Loc">{props.rating} {props.location}</span>
-                 <p>{props.name}</p>
-                 <p>{props.price} / person</p>                  
+                 <div className="Card--details">
+                    <img className="Image--star" src ="../image/Star.png" alt="star" />  
+                    <span className="Rating--Loc">{props.rating} {props.location}</span> 
+                
+                    <label className="Name--Cards">{props.name}</label>
+                    <label className="Price">{props.price} / person</label>               
                 </div> 
+                
                  
             </section>
             
