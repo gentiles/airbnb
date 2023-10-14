28a0036a091c1c6a0bb97c244cb9589a6723c754
erwinmarin05@gmail.com <erwinmarin05@gmail.com>
2023-10-15 05:04:56 +0800

    Hero section Done

---
+++ src/components/Cards.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Cards (){
+    return(
+        <div>
+            <h2>Cards here</h2>
+        </div>
+    )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar(){
     return(
         <div>
                 <section className="NavBar">
-                  <img className="NavBar--logo" src="../image/airbnb.png" />
+                  <img className="NavBar--logo" src="../image/airbnb.png" alt="logo" />
                  
                 </section>
         </div>
